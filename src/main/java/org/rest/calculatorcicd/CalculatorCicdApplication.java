a5055ce1548be036d6e567d0fb65406dbb2dc90f
Restu Maulunida <vandalz12@gmail.com>
2023-11-17 09:20:15 +0700

    Add Spring Boot skeleton

---
+++ src/main/java/org/rest/calculatorcicd/CalculatorCicdApplication.java
@@ -0,0 +1,13 @@
+package org.rest.calculatorcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rest/calculatorcicd/CalculatorCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rest.calculatorcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
